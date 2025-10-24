f1a2bff257c8e0138da7fd869d20fc4115b738af
Jon C <me@jonc.dev>
2025-10-24 15:46:50 +0200

    program: Update to sdkv3 (#120)
    #### Problem
    
    The program is still on v2 of the SDK, but v3 has been out for some
    time, and we need a crate compatible with v3 for token-2022.
    
    #### Summary of changes
    
    Update everything SDKv3 crates, use split-up crates in tests.

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -10,8 +10,8 @@ pub mod state;
 // Export current SDK types for downstream users building with a different SDK
 // version
 pub use {
-    solana_account_info, solana_decode_error, solana_instruction, solana_msg,
-    solana_program_entrypoint, solana_program_error, solana_program_pack, solana_pubkey,
+    solana_account_info, solana_instruction, solana_msg, solana_program_entrypoint,
+    solana_program_error, solana_program_pack, solana_pubkey,
 };
 
 solana_pubkey::declare_id!("recr1L3PCGKLbckBqMNcJhuuyU1zgo8nBhfLVsJNwr5");
