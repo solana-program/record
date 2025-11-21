2e35c0ff53e78073d6b5e4ae4ffe6d61cb67b9ac
samkim-crypto <skim13@cs.stanford.edu>
2025-11-21 16:51:11 +0900

    [js-client] Add `idl.json` for the program and add js clients (#129)
    add `idl.json` for the program and add js clients

---
+++ clients/js/src/constants.ts
@@ -0,0 +1,8 @@
+/** A record account size excluding the record payload */
+export const RECORD_META_DATA_SIZE = 33n;
+
+/** Maximum record chunk that can fit inside a transaction when initializing a record account */
+export const RECORD_CHUNK_SIZE_PRE_INITIALIZE = 696;
+
+/** Maximum record chunk that can fit inside a transaction when record account already initialized */
+export const RECORD_CHUNK_SIZE_POST_INITIALIZE = 917;
